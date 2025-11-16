aaa626493eb047fe229c23dd509ded4bb1429edf
Kkaif4 <kkaifshaikh.27@gamil.com>
2025-11-15 19:16:27 -0500

    style(login page): fixed login page styling

--- src/components/common/logo.tsx
+++ src/components/common/logo.tsx
@@ -3,6 +3,6 @@ export const Logo = () => (
     <div className="w-12 h-12 bg-teal-500 rounded-lg flex items-center justify-center mr-3">
       <span className="text-white font-bold text-xl">RH</span>
     </div>
-    <h1 className="text-2xl font-bold text-slate-900">POS</h1>
+    <h1 className="text-2xl font-bold">POS</h1>
   </div>
 );
