51a726e76fcec9bfe3d20b12815313d404de7aaf
Kkaif4 <kkaifshaikh.27@gamil.com>
2025-11-16 13:58:39 -0500

    fix(auth): fixed forgot password
    forgot password page styling

--- src/components/common/logo.tsx
+++ src/components/common/logo.tsx
@@ -1,7 +1,14 @@
 export const Logo = () => (
   <div className="flex items-center justify-center mb-8">
-    <div className="w-12 h-12 bg-teal-500 rounded-lg flex items-center justify-center mr-3">
+    <div className="w-12 h-12 bg-primary rounded-lg flex items-center justify-center mr-3">
       <span className="text-white font-bold text-xl">RH</span>
+
+      {/* will add logo later */}
+      {/* <img
+        src="/logo.png" // Use direct path for public folder, or LogoImage for src folder
+        alt="Restaurant POS Logo"
+        className="w-12 h-12 mr-3" // You can adjust width, height, and margin as needed
+      /> */}
     </div>
     <h1 className="text-2xl font-bold">POS</h1>
   </div>
